af1d1344f1667423719aa9b22b14e3ebf3a36aad
Kuldeep-Clevrr <kuldeep.singh@getclevrr.com>
2026-03-22 00:19:45 +0530

    vercel fix

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -7,6 +7,9 @@ const __dirname = path.dirname(__filename);
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typedRoutes: false,
+  experimental: {
+    externalDir: true
+  },
   webpack: (config) => {
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
